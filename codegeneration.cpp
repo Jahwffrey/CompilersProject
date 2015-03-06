5cb53b708af8327b9b387459960fa5912ac21be2
John Thomason <johnthomason@umail.ucsb.edu>
2015-03-05 18:53:41 -0800

    Low hanging fruit got

--- codegeneration.cpp
+++ codegeneration.cpp
@@ -1,145 +1,246 @@
 #include "codegeneration.hpp"
+using namespace std;
 
 // CodeGenerator Visitor Functions: These are the functions
 // you will complete to generate the x86 assembly code. Not
 // all functions must have code, many may be left empty.
 
 void CodeGenerator::visitProgramNode(ProgramNode* node) {
-    // WRITEME: Replace with code if necessary
+	
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitClassNode(ClassNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitMethodNode(MethodNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitMethodBodyNode(MethodBodyNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitParameterNode(ParameterNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitDeclarationNode(DeclarationNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitReturnStatementNode(ReturnStatementNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitAssignmentNode(AssignmentNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitCallNode(CallNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitIfElseNode(IfElseNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Ifelsenode";
+	int tempLabel = nextLabel();
+	int tempLabel2 = nextLabel();
+	//Want to visit children in a specific order. First visit the expression, then t/f is on top of stack. If equal to true, jumpt to the true block. else continue into
+	//the false block and then skip past the true block
+	node->expression->accept(this);
+	cout << "POP %EAX\n";
+	cout << "CMP %EAX,$1\n";
+	cout << "JE label_" << tempLabel << "\n";
+	if (node->statement_list_2) {
+		for(std::list<StatementNode*>::iterator iter = node->statement_list_2->begin();
+		iter != node->statement_list_2->end(); iter++) {
+			(*iter)->accept(this);
+    		}
+	}
+	cout << "JMP label_" << tempLabel2 << "\n";
+	cout << "label_" << tempLabel << ":\n";
+	if (node->statement_list_1) {
+		for(std::list<StatementNode*>::iterator iter = node->statement_list_1->begin();
+		iter != node->statement_list_1->end(); iter++) {
+			(*iter)->accept(this);
+    		}
+	}
+	cout << "label_" << tempLabel2 << ":\n";
 }
 
 void CodeGenerator::visitWhileNode(WhileNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitPrintNode(PrintNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitPlusNode(PlusNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Addnode\n";
+	node->visit_children(this);
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "ADD %EDX,%EAX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitMinusNode(MinusNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Minusnode\n";
+	node->visit_children(this);
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "SUB %EDX,%EAX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitTimesNode(TimesNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Timesnode\n";
+	node->visit_children(this);
+	cout << "POP %EBX\n";
+	cout << "POP %EAX\n";
+	cout << "CDQ\n";
+	cout << "IMUL %EBX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitDivideNode(DivideNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Dividenode\n";
+	node->visit_children(this);
+	cout << "POP %EBX\n";
+	cout << "POP %EAX\n";
+	cout << "CDQ\n";
+	cout << "IDIV %EBX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitLessNode(LessNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Lessnode\n";
+	node->visit_children(this);
+	int label1 = nextLabel();
+	int label2 = nextLabel();
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "CMP %EAX,%EDX\n";
+	cout << "JL label_" << label1 << "\n";
+	cout << "PUSH $0" << "\n";
+	cout << "JMP label_" << label2 << "\n";
+	cout << "label_" << label1 << ":\n";
+	cout << "PUSH $1\n";
+	cout << "label_" <<label2 << "\n";
 }
 
 void CodeGenerator::visitLessEqualNode(LessEqualNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Lessequalnode\n";
+	node->visit_children(this);
+	int label1 = nextLabel();
+	int label2 = nextLabel();
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "CMP %EAX,%EDX\n";
+	cout << "JEL label_" << label1 << "\n";
+	cout << "PUSH $0\n";
+	cout << "JMP label_" << label2 << "\n";
+	cout << "label_" << label1 << ":\n";
+	cout << "PUSH $1\n";
+	cout << "label_" <<label2 << "\n";
 }
 
 void CodeGenerator::visitEqualNode(EqualNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Equalnode\n";
+	node->visit_children(this);
+	int label1 = nextLabel();
+	int label2 = nextLabel();
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "CMP %EAX,%EDX\n";
+	cout << "JZ label_" << label1 << "\n";
+	cout << "PUSH $0" << "\n";
+	cout << "JMP label_" << label2 << "\n";
+	cout << "label_" << label1 << ":\n";
+	cout << "PUSH $1\n";
+	cout << "label_" <<label2 << "\n";
 }
 
 void CodeGenerator::visitAndNode(AndNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Andnode\n"; 
+	node->visit_children(this);
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "AND %EDX,%EAX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitOrNode(OrNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Ornode\n";
+	node->visit_children(this);
+	cout << "POP %EDX\n";
+	cout << "POP %EAX\n";
+	cout << "OR %EDX,%EAX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitNotNode(NotNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Notnode\n";
+	node->visit_children(this);
+	//Flip the first bit of a boolean
+	cout << "POP %EAX\n";
+	cout << "XOR $1,%EAX\n";
+	cout << "PUSH %EAX\n";
 }
 
 void CodeGenerator::visitNegationNode(NegationNode* node) {
-    // WRITEME: Replace with code if necessary
+	cout << "#-- Negationnode\n";
+	node->visit_children(this);
+	cout << "NEG %ESP\n";
 }
 
 void CodeGenerator::visitMethodCallNode(MethodCallNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitMemberAccessNode(MemberAccessNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitVariableNode(VariableNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitIntegerLiteralNode(IntegerLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
+	cout << "PUSH $" << node->integer->value << "\n";
 }
 
 void CodeGenerator::visitBooleanLiteralNode(BooleanLiteralNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitNewNode(NewNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitIntegerTypeNode(IntegerTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitBooleanTypeNode(BooleanTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitObjectTypeNode(ObjectTypeNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitNoneNode(NoneNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitIdentifierNode(IdentifierNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
 
 void CodeGenerator::visitIntegerNode(IntegerNode* node) {
-    // WRITEME: Replace with code if necessary
+	node->visit_children(this);
 }
